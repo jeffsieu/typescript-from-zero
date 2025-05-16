bbfacfb5341a97fcb4cc497295f27959466644a2
Jeff Sieu <jeffsy00@gmail.com>
2025-05-17 00:06:15 +0800

    feat: edit tagline

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "TypeScript from Zero",
-  tagline: "Learn advanced TypeScript from solid foundations",
+  tagline: "Learn advanced TypeScript starting from solid foundations",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
