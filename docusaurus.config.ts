9fd3888fc21a934bac23f280b7e7863adb7c1d4d
Jeff Sieu <jeffsy00@gmail.com>
2025-05-17 00:06:47 +0800

    fix: update copyright with my name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -96,7 +96,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Jeff Sieu. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
