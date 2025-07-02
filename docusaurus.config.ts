43cbb397aa6a6049891b645780b2ee3c846a7ce6
Jeff Sieu <jeffsy00@gmail.com>
2025-07-02 23:20:05 +0800

    feat: change baseUrl back to /

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: "https://jeffsieu.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/typescript-from-zero/",
+  baseUrl: "/",
 
   organizationName: "jeffsieu",
   projectName: "typescript-from-zero",
